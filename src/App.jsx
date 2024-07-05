c535ad4b6698c621ad3b0d08835409eac218948c
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-07-04 18:06:51 -0600

    Basic structure octotimer html

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react';
+import octoLogo from './assets/octo_1_2.png';
+import textLogo_White from './assets/logo_texto_2.png';
+import svg_Left from './assets/svg/left.svg';
+import svg_User from './assets/svg/user.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="hero">
+        <img src={octoLogo} alt="Octotimer logo" />
+        <h2>15:00</h2>
+        <section className="hero__instructions">
+          <div>
+            <p>Instructions</p>
+            <input type="text" />
+            <button type="button">Iniciar Temporizador</button>
+          </div>
+          <div>
+            <p>Mensaje al finalizar el tiempo</p>
+            <button type="button">Vuelvan a la sala</button>
+          </div>
+          <div>
+            <div>
+              <p>Número de usuarios finales conectados al timer</p>
+              <p>5</p>
+              <img src={svg_User} alt="User Icon svg" />
+            </div>
+          </div>
+        </section>
+      </main>
+      <aside className="aside">
+        <div>
+          <img src={svg_Left} alt="Left arrow svg" />
+        </div>
+      </aside>
+      <footer className="footer">
+        <img src={textLogo_White} alt="Octotimer logo white" />
+      </footer>
     </>
   );
 }
